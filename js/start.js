cb88fdf38d3f64cdd79ac7e7fe229364b680cc5c
nayeongdev <nayeongkim.dev@gmail.com>
2022-09-21 00:56:03 +0900

    220920-2

--- js/start.js
+++ js/start.js
@@ -2,47 +2,11 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const reslut = document.querySelector("#result");
 const endPoint = 12;
-const select = [];
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function calResult() {
-  var pointArray = [
-    { name: 'mouse', value: 0, key: 0 },
-    { name: 'cow', value: 0, key: 1 },
-    { name: 'tiger', value: 0, key: 2 },
-    { name: 'rabbit', value: 0, key: 3 },
-    { name: 'dragon', value: 0, key: 4 },
-    { name: 'snake', value: 0, key: 5 },
-    { name: 'horse', value: 0, key: 6 },
-    { name: 'sheep', value: 0, key: 7 },
-    { name: 'monkey', value: 0, key: 8 },
-    { name: 'chick', value: 0, key: 9 },
-    { name: 'dog', value: 0, key: 10 },
-    { name: 'pig', value: 0, key: 11 }
-  ]
-
-  for (let i = 0; i < endPoint; i++) {
-    var target = qnaList[i].a[select[i]];
-    for (let j = 0; j < target.type.length; j++) {
-      for (let k = 0; k < pointArray.length; k++) {
-        if (target.type[j] === pointArray[k].name) {
-          pointArray[k].value += 1;
-        }
-      }
-    }
-  }
-  
-  var reslutArray = pointArray.sort(function (a, b) {
-    if (a.value > b.value) {
-      return -1;
-    }
-    if (a.value < b.value) {
-      return 1;
-    }
-    return 0;
-  });
-  console.log(reslutArray);
-  let reslutworld = reslutArray[0].key;
-  return reslutworld;
+  var reslut = select.indexOf(Math.max(...select));
+  return reslut;
 }
 
 function goResult() {
@@ -81,7 +45,11 @@ function addAnswer(answerText, qIdx, idx) {
       children[i].style.animation = "fadeOut 0.5s";
     }
     setTimeout(() => {
-      select[qIdx] = idx;
+      var target = qnaList[qIdx].a[idx].type;
+      for (let i = 0; i < target.length; i++) {
+        select[target[i]] += 1;
+      }
+
       for (let i = 0; i < children.length; i++) {
         children[i].style.display = 'none';
       }
